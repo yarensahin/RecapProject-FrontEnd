0225d468fabf2abda2d6d02a5e377351300217bf
YAREN ŞAHİN <l5817021@std.yildiz.edu.tr>
2021-03-14 02:20:40 +0300

    CarRentalProject

---
+++ src/app/models/car.ts
@@ -0,0 +1,9 @@
+export interface Car{
+    carName:string;   
+    brandName:string;
+    colorName:string;
+    dailyPrice:number;   
+    description:string;
+    modelYear:number;
+    id:number;
+}
